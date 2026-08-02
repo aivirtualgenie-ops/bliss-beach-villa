8706a5c978e0f8683fcf213656941d20f01f4579
aivirtualgenie-ops <aivirtualgenie@gmail.com>
2026-08-03 00:52:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -182,3 +182,20 @@ hero.style.transform="translateY(0)";
 },300);
 
 });
+const roomSlides = document.querySelectorAll(".room-slide");
+
+if (roomSlides.length > 0) {
+
+    let currentSlide = 0;
+
+    setInterval(() => {
+
+        roomSlides[currentSlide].classList.remove("active");
+
+        currentSlide = (currentSlide + 1) % roomSlides.length;
+
+        roomSlides[currentSlide].classList.add("active");
+
+    }, 4000);
+
+}
